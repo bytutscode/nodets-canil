a35122eb5777e42ba26014ff7aa88113d708d351
Samuel Lima <samuellimadossantosoficial@gmail.com>
2023-01-14 22:10:55 -0300

    feito os models e os controls necessarios

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -2,37 +2,47 @@ import {Request,Response} from 'express';
 
 import { createMenuObject } from '../helpers/createMenuObject';
 
+import { pet } from '../models/pet';
+
 
 const home = (req: Request, res: Response)=>{
-    res.render('pages/mainPage',{menu:createMenuObject('all'),
+    res.render('pages/mainPage',{
+        menu:createMenuObject('all'),
         banner:{
             title:'Todos animais',
             background:'allanimals.jpg'
-        }
+        },
+        petList:pet.getAll()
     });
 }
 const dogs = (req: Request, res: Response)=>{
-    res.render('pages/mainPage',{menu:createMenuObject('dog'),
+    res.render('pages/mainPage',
+    {menu:createMenuObject('dog'),
         banner:{
             title:'Todos cachorros',
             background:'banner_dog.jpg'
-        }
+        },
+        petList:pet.getAllTypeOf('dog')
     });
 }
 const cats = (req: Request, res: Response)=>{
-    res.render('pages/mainPage',{menu:createMenuObject('cat'),
+    res.render('pages/mainPage',{
+        menu:createMenuObject('cat'),
         banner:{
             title:'Todos gatos',
             background:'banner_cat.jpg'
-        }
+        },
+        petList:pet.getAllTypeOf('cat')
     });
 }
 const fishes = (req: Request, res: Response)=>{
-    res.render('pages/mainPage',{menu:createMenuObject('fish'),
+    res.render('pages/mainPage',{
+        menu:createMenuObject('fish'),
         banner:{
             title:'Todos peixes',
             background:'banner_fish.jpg'
-        }
+        },
+        petList:pet.getAllTypeOf('fish')
     });
 }
 
